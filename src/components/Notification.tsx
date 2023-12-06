107651bd8a633e0f82d560bd8a506007ff6c2e23
Jerud-Moyer <jerudmoyer@gmail.com>
2023-12-05 21:57:51 -0800

    add notification component

---
+++ src/components/Notification.tsx
@@ -0,0 +1,53 @@
+import { AlertProps, Slide, Snackbar } from '@mui/material'
+import MuiAlert from '@mui/material/Alert'
+import React, { forwardRef, useEffect, useState } from 'react'
+
+interface Props {
+  severity: 'success' | 'warning';
+  message: string;
+}
+
+function Notification({severity, message}: Props) {
+  const [show, setShow] = useState<boolean>(false)
+
+  const slide = (props: any) => {
+    return <Slide {...props} direction='up' />
+  }
+
+  const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
+  })
+
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  useEffect(() => {
+    setShow(true)
+  }, [message])
+
+
+  return (
+    <div>
+      <Snackbar
+        open={show}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        TransitionComponent={slide}
+      >
+        <Alert
+          severity={severity}
+          onClose={handleClose}
+          sx={{width: '100%'}}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
+  )
+}
+
+export default Notification
