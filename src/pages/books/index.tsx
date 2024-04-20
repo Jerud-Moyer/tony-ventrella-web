8939f932f6a669932f93c0bff7e9a9d81d5a6782
Jerud-Moyer <jerudmoyer@gmail.com>
2024-04-20 16:14:53 -0700

    add some changes to books

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,18 +1,77 @@
 import SubPageLayout from '@/components/SubPageLayout'
-import React from 'react'
+import Image from 'next/image'
+import React, { useEffect, useRef, useState } from 'react'
 
-function index() {
+function Index() {
+  const imageOneRef = useRef(null)
+  const [showBooks, setShowBooks] = useState<boolean>(false)
+
+  useEffect(() => {
+    if(window !== undefined) {
+      const picObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if(entry.isIntersecting) {
+            console.log('intersecting! ', entry)
+          } else {  
+            console.log('not intersecting ', entry)
+          }
+        })
+      })
+      
+      if(imageOneRef.current) {
+        picObserver.observe(imageOneRef.current)
+      }
+
+      setTimeout(() => {
+        setShowBooks(true)
+      }, 1500)
+    }
+  }, [imageOneRef])
+
   return (
     <div>
       <SubPageLayout typeoutMessage='the printed word'>
-        <p
-          className='text-eerie_black text-3xl text-center'
+      <p
+          className='font-go_bold text-eerie_black text-6xl text-center mb-24 animate-gold_rush glow'
         >
-          Information on books coming soon!
+          New Book Coming Soon!
         </p>
+        {showBooks &&
+          <>
+            <p className="text-eerie_black text-3xl m-12">
+              Previous Writings:
+            </p>
+            <div className="flex flex-row gap-16 flex-wrap justify-around">
+              <Image 
+                height={400}
+                width={400} 
+                src='/images/smile-in-the-mirror.jpeg'
+                alt='book cover'
+                ref={imageOneRef}
+                className={`animate-pop`}
+              />
+              <Image 
+                height={400}
+                width={400} 
+                src='/images/smile-in-the-mirror-3rd.jpeg'
+                alt='book cover'
+                ref={imageOneRef}
+                className={`animate-pop`}
+              />
+              <Image 
+                height={400}
+                width={400} 
+                src='/images/heres-smiling-at-you.jpeg'
+                alt='book cover'
+                ref={imageOneRef}
+                className={`animate-pop`}
+              />
+            </div>
+          </>
+        }
       </SubPageLayout>
     </div>
   )
 }
 
-export default index
+export default Index
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        go_bold: ["go-bold", 'sans-serif']
+      },
       colors: {
         soft_white: '#f7f9f9',
         dark_green: '#44633f',
@@ -35,13 +38,54 @@ module.exports = {
           '0%': {transform: 'translateY(300px) translateX(100px) rotate(-12deg)'},
           '40%': {transform: 'translateY(300px)'},
           '100%': {transform: 'translateY(-155px) translateX(120px) rotate(-12deg)'}
-        }
+        },
+        pop: {
+          '0%': {
+            transform: 'scale(0)', 
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'scale(1)',
+            opacity: '1'
+          }
+        },
+        gold_rush: {
+          '0%': {
+            color: '#111910',
+            textShadow: '5px 5px 10px #d8ba5e',
+            transform: 'scale(1.5)'
+          },
+          '25%': {
+            // color: '#d8ba5e',
+            textShadow: '5px 5px 10px #cba328'
+          },
+          // '40%': {
+          //   color: '#111910',
+          //   textShadow: '5px 5px 10px #d8ba5e'
+          // },
+          '50%': {
+            color: '#d8ba5e',
+            textShadow: '5px 5px 10px #111910',
+            transform: 'scale(1.5)'
+          },
+          '75%': {
+            // color: '#d8ba5e',
+            textShadow: '5px 5px 10px #cba328'
+          },
+          '100%': {
+            color: '#111910',
+            textShadow: '5px 5px 10px #d8ba5e',
+            transform: 'scale(1)'
+          }
+        }       
       },
       animation: {
         fade: 'fade .8s ease',
         image_fade: 'fade 2s ease',
         rise: 'rise 1.2s ease',
-        rise_later: 'rise_later 1.5s ease'
+        rise_later: 'rise_later 1.5s ease',
+        pop: 'pop .5s ease',
+        gold_rush: 'gold_rush 1.5s ease'
       }
     },
   },
